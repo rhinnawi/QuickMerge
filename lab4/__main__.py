a6b362552bdbd24abe01127674be84515731a383
Rani Hinnawi <rhinnawi95@gmail.com>
2023-08-17 07:10:02 -0400

    Set up basic runner file

--- lab4/__main__.py
+++ lab4/__main__.py
@@ -17,7 +17,7 @@ from sys import stderr
 from pathlib import Path
 import argparse
 from support.is_valid_io import is_valid_io
-from hencoding.run import run
+from lab4.run import run
 
 DEFAULT_FREQUENCY_TABLE_PATH = "hencoding/DefaultFreqTable.txt"
 
@@ -25,32 +25,20 @@ DEFAULT_FREQUENCY_TABLE_PATH = "hencoding/DefaultFreqTable.txt"
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("input_file", type=str, help="Input file pathname")
 arg_parser.add_argument("output_file", type=str, help="Output file pathname")
-arg_parser.add_argument("--frequency_table", type=str,
-                        help="(Optional) Frequency table file pathname")
-arg_parser.add_argument("--memoize", action="store_true",
-                        help="Toggles on memoization for encoding")
 arg_parser.add_argument("--debug", action="store_true",
                         help="Toggles debug mode to log errors to stderr")
-
-# Either --encode or --decode may be passed in. Not both nor neither
-group = arg_parser.add_mutually_exclusive_group(required=True)
-group.add_argument("--encode", action="store_true",
-                   help="Indicates to encode input file strings")
-group.add_argument("--decode", action="store_true",
-                   help="Indicates to decode input file strings")
 args = arg_parser.parse_args()
 
 # Convert file names into paths
 in_file = Path(args.input_file)
 out_file = Path(args.output_file)
-freq_table = Path(args.frequency_table) if args.frequency_table else Path(
-    DEFAULT_FREQUENCY_TABLE_PATH)
 
 # Validate file paths then run main program
 try:
-    is_valid_io(in_file, out_file, freq_table)
-    run(freq_table, in_file, out_file, encode=args.encode,
-        memo=args.memoize, decode=args.decode, debug=args.debug)
+    is_valid_io(in_file, out_file)
+    run()
+    # TODO: Replace run(freq_table, in_file, out_file, encode=args.encode,
+    #    memo=args.memoize, decode=args.decode, debug=args.debug)
 except FileNotFoundError as fnfe:
     error_message = fnfe.args[0]
     if args.debug:
---
+++ lab4/run.py
@@ -0,0 +1,22 @@
+"""
+run
+
+This module contains the primary function for QuickMerge. It assumes valid I/O.
+While running, it logs performance metrics. All results are written to the 
+output file.
+
+Author: Rani Hinnawi
+Date: 2023-08-22
+"""
+def run():
+    """
+    Wrapper function for running Quicksort and Natural Merge Sort using input
+    file data, then writing results and performance metrics to output file.
+
+    Args:
+        input_file (TextIO): text file with string items to sort
+        output_file (TextIO): text file where results are written
+        debug (bool): True if debug mode is toggled on, otherwise False
+    """
+    print("OK")
+    
