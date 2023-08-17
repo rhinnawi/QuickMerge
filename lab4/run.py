bec0737fd20fd03daea1a6eafc59677656978d83
Rani Hinnawi <rhinnawi95@gmail.com>
2023-08-17 11:42:36 -0400

    set up runner module

--- lab4/run.py
+++ lab4/run.py
@@ -8,9 +8,13 @@ output file.
 Author: Rani Hinnawi
 Date: 2023-08-22
 """
+from sys import stderr
 from typing import TextIO
+from support.output_formatters import write_to_output
+from support.performance import Performance
 
-def run(input_file: TextIO, output_file: TextIO):
+
+def run(input_file: TextIO, output_file: TextIO, debug=False):
     """
     Wrapper function for running Quicksort and Natural Merge Sort using input
     file data, then writing results and performance metrics to output file.
@@ -20,8 +24,52 @@ def run(input_file: TextIO, output_file: TextIO):
         output_file (TextIO): text file where results are written
         debug (bool): True if debug mode is toggled on, otherwise False
     """
-    with open(input_file, "r", encoding="utf-8") as inputs, open(output_file, "w", encoding="utf-8") as out:
+    # Set up Performance object and output strings used by runner functions
+    performance = Performance()
+    out = []
+
+    with open(input_file, 'r', encoding="utf-8") as inputs:
+        out.append("\n\n-------Sorting Results-------\n")
+        line_counter = 1
+        # TODO: replace for-loop with cleaner way to read individual records
         for line in inputs:
-            print(line.split())
-            out.write(str(line.split()))
-    print("OK")
+            expression = line.strip()
+            size = len(expression)
+
+            if size == 0:
+                # Case: empty line. Ignore.
+                continue
+
+            # Set up error handling and performance metrics
+            error = False
+            result = ""
+            performance.set_size(size).start()
+
+            try:
+                result = print(expression)
+            except ValueError as ve:
+                result = ve.args[0]
+                error = True
+
+                if debug:
+                    error_message = f"Expression: {expression}"
+                    error_message += f"\n\tError Message: {result}"
+                    print(error_message, file=stderr)
+            finally:
+                performance.stop()
+
+                if error:
+                    performance.log_error()
+                else:
+                    performance.log_success()
+
+                line_counter += 1
+
+    # Display conversion values
+    out.append("\nSorted values: ")
+
+    # Output results
+    write_to_output(output_file, out)
+
+    if debug:
+        print('OK', file=stderr)
